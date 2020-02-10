ccf742e47f5b7b0660164d9475ecd6e3f73c8864
Jeff Takaki <jeffrey.takaki@yahoo.com>
2020-02-10 15:41:18 -0800

    add redux dev tool

--- server/client/src/store.js
+++ server/client/src/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 const middleWare = [thunk]; // thunk allows us to make async/await calls in the action creators
 
-const store = createStore(rootReducer, applyMiddleware(...middleWare));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleWare)));
 
 export default store;
