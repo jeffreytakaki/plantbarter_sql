058284884a35dc0fa9a5b51ac96604d235ba9b2b
Jeff Takaki <jeffrey.takaki@yahoo.com>
2020-02-10 15:56:49 -0800

    add react router

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HeaderConnected from './components/Header';
+import Home from './pages/Home'
 import PlantCollection from './components/Plant-Collection';
 import PlantForm from './components/Plant-Form'
 import {Provider} from 'react-redux';
 import store from './store';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 
 function App() {
@@ -13,8 +20,18 @@ function App() {
         <Provider store={store}>
             <div className="page-container">
                 <HeaderConnected />
-                <PlantForm />
-                <PlantCollection />
+                <Router>
+                <Route path='/home'>
+                        <Home />
+                    </Route>
+                    <Route path='/plantform'>
+                        <PlantForm />
+                    </Route>
+                    <Route path='/plant-collection'>
+                        <PlantCollection />
+                    </Route>
+                    
+                </Router>
             </div>
         </Provider>
     );
---
+++ server/client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+    constructor(props, dispatch) {
+        super(props);
+       
+    }
+
+    
+
+    render() {
+        return (
+
+            <h1>home</h1>
+
+        )
+    }
+}
