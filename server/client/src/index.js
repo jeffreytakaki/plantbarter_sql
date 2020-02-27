e60495afa99bc55fa58d0340698eaa36316131c3
Jeff Takaki <jeffrey.takaki@yahoo.com>
2020-02-26 18:44:48 -0800

    update docker file, update package json scripts, fix app scss reference.

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import './scss/App.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.scss';
 
